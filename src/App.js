016907d5139f8fa7a4c12f761d3a980785407e55
Shihab Uddin <56557098+wonderkidshihab@users.noreply.github.com>
2023-04-03 17:36:54 +0000

    text added

--- src/App.js
+++ src/App.js
@@ -3,25 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h1>I love you Mayesha.</h1>
+      <span role="img" aria-label="love"
+        style={
+          {
+            fontSize: '100px',
+            color: 'red'
+          }
+        }
+      >❤️</span>
     </div>
   );
 }
