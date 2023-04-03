0002ad9482e00545fda57aaceaee766afcb3b503
Shihab Uddin <56557098+wonderkidshihab@users.noreply.github.com>
2023-04-03 18:09:35 +0000

    Fresh start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>I love you Mayesha.</h1>
-      <span role="img" aria-label="love"
-        style={
-          {
-            fontSize: '100px',
-            color: 'red'
-          }
-        }
-      >❤️</span>
-    </div>
+    <>
+      
+    </>
   );
 }
 
